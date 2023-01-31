098f5fd8ae6b3c73e36ffe895f88790ca8f5ad9a
Sergey Ovcharenko <sergey@terminal.io>
2023-01-31 14:03:53 -0500

    update projects

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,18 +1,19 @@
 module.exports = {
     siteMetadata: {
-      description: "Frontend Developer from London, Ontario, Canada. React Native apps, Next.js, Custom WordPress themes.",
-      locale: "en",
-      title: "Sergey Ovcharenko. Frontend Developer",
+        description:
+            'Frontend Developer from London, Ontario, Canada. React Native apps, Next.js, Custom WordPress themes.',
+        locale: 'en',
+        title: 'Serhii Ovcharenko. Frontend Developer',
     },
     plugins: [
-      {
-        resolve: "@wkocjan/gatsby-theme-intro",
-        options: {
-          basePath: "/",
-          contentPath: "content/",
-          showThemeLogo: false,
-          theme: "dark-pink",
+        {
+            resolve: '@wkocjan/gatsby-theme-intro',
+            options: {
+                basePath: '/',
+                contentPath: 'content/',
+                showThemeLogo: false,
+                theme: 'dark-pink',
+            },
         },
-      },
     ],
-  }
+};
